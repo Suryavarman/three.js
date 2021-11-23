76885706f75899eac91c8539b4683f35795fcfb7
Pierre <Suryavarman@users.noreply.github.com>
2021-11-24 00:53:35 +0100

    import opentype.js return undefined
    To fix that you have to change this line
    import { opentype } from '../libs/opentype.module.min.js';
    by this line
    import * as opentype from '../libs/opentype.module.min.js';

--- examples/jsm/loaders/TTFLoader.js
+++ examples/jsm/loaders/TTFLoader.js
@@ -2,7 +2,7 @@ import {
 	FileLoader,
 	Loader
 } from '../../../build/three.module.js';
-import { opentype } from '../libs/opentype.module.min.js';
+import * as opentype from '../libs/opentype.module.min.js';
 
 /**
  * Requires opentype.js to be included in the project.
